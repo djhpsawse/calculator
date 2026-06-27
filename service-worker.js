5cd409506fed86837a4509ce05778ac2d6ff6a25
djhpsawse <harryprior@btinternet.com>
2026-06-27 15:56:06 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,21 @@
-const CACHE_NAME = "money-pot-cache-v2";
+const CACHE_NAME = "money-pot-cache-v3";
 
 const assetsToCache = [
   "./",
-  "index.html",
-  "manifest.json",
-  "img/gbp.jpg",
-  "img/eur.jpg"
+  "./index.html",
+  "./manifest.json",
+  "./img/gbp.jpg",
+  "./img/eur.jpg"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assetsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assetsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
